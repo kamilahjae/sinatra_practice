cbf17112bf61efdb6fd8091982eab5b6c392e4ef
KamilahJ4e <kamilahjae@gmail.com>
2014-09-25 10:31:47 -0700

    added attributes to Post

--- lib/post.rb
+++ lib/post.rb
@@ -6,11 +6,15 @@ class Post
   # Create an array of post objects
   # with the attributes set
   # Post.all => [post, post] and each post obj has attributes
+  attr_accessor :title, :url
+
   def initialize(array)
+    @title = array[-1].split(".")[0] #title
+    @url   = "/posts/#{@title}"
 
   end
   def self.all
-    Dir.glob("views/posts/*").each do |filepath|
+    Dir.glob("views/posts/*").collect do |filepath|
       a = filepath.split
       new(a)
     end
