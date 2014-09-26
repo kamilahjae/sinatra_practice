7d8c2d08516f3aacb5ea1c7979d704e921478489
KamilahJ4e <kamilahjae@gmail.com>
2014-09-26 14:04:01 -0700

    Added header functionality.

--- my_app.rb
+++ my_app.rb
@@ -13,6 +13,11 @@ class MyApp < Sinatra::Base
     erb :index # render erb
   end
 
+  post "/" do
+    puts params.inspect #add input into database
+    redirect to("/") # #2 in chart: this will end us back tot he get "/" request
+  end
+
   get "/about-me" do
     erb :about_me
   end
@@ -23,3 +28,5 @@ class MyApp < Sinatra::Base
     erb "/posts/#{post}".to_sym
   end
 end
+
+# get modifies the url so that everything after the question mark is the body
